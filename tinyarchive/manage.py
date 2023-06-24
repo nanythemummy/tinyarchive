83de29f49e3d59b16f5812e956d2d339a8f79abe
Kea <kjohnsto@berkeley.edu>
2023-06-24 16:22:04 -0700

    Added support for attaching a vscode to django running in debugger to docker container.

--- tinyarchive/manage.py
+++ tinyarchive/manage.py
@@ -7,6 +7,14 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinyarchive.settings')
+
+    from django.conf import settings
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN'):
+            import debugpy
+            debugpy.listen(("0.0.0.0",5678))
+            print('Attatched')
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
