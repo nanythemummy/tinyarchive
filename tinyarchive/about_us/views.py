0808f9a201bbaa27509d560a62b460990247ac82
Noah David <noahdavid@berkeley.edu>
2023-08-03 12:48:29 -0700

    deleted the archive style about us page, it was more complex than it needed to be. it only needs to be a basic static page, without a database schema

--- tinyarchive/about_us/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# This is just the view for the static about.
-def about_us(request):
-    context = {}
-    #return render(request, "about_us/about_us.html", {})
-    return render(request, "about_us.html", {})
