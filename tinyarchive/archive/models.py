234ebbeea798c1dfbad6dbc92fbbcbb77271b055
Peiying Guan <etett08042@berkeley.edu>
2023-08-04 02:27:39 +0800

    added a Sculpture model

--- tinyarchive/archive/models.py
+++ tinyarchive/archive/models.py
@@ -40,19 +40,19 @@ class AssociatedImage(models.Model):
     def __str__(self):
         return(self.photo_image.url)
 
-# class AudioRecording(ArchiveDocument):
-#     language = models.CharField(max_length=200)
-#     speaker = models.CharField(max_length=200, blank = True)
-#     recording_date = models.DateField(auto_now=True)
-#     audio_file = models.FileField(upload_to="sounds/", null=True)
+class AudioRecording(ArchiveDocument):
+    language = models.CharField(max_length=200)
+    speaker = models.CharField(max_length=200, blank = True)
+    recording_date = models.DateField(auto_now=True)
+    audio_file = models.FileField(upload_to="sounds/", null=True)
     
-# class Photograph(ArchiveDocument):
-#     photo_type = models.CharField(
-#         max_length=20,
-#         choices=list(
-#             Choices.PHOTO_TYPE_CHOICES.items()
-#         ),  # defining the constant as a dictionary for easy lookup in views.
-#     )
+class Photograph(ArchiveDocument):
+    photo_type = models.CharField(
+        max_length=20,
+        choices=list(
+            Choices.PHOTO_TYPE_CHOICES.items()
+        ),  # defining the constant as a dictionary for easy lookup in views.
+    )
 
 
 class Artifact(ArchiveDocument):
@@ -68,19 +68,32 @@ class Artifact(ArchiveDocument):
                         (MAT_GLASS, "Glass"),
                         (MAT_METAL,"Metal")]
     material = models.CharField(
-        max_length=50, choices=MATERIAL_CHOICES, default=MAT_METAL)
+        max_length=50, choices=MATERIAL_CHOICES, default=MAT_GLASS)   
+    model3d = models.URLField(max_length=500, blank="True")
+
+
+class Document(ArchiveDocument):
+    # might want to do something to standardize this later so people can't
+    # just enter variant spellings for language names--a preformated list of standard names
+    # and codes?
+    language = models.CharField(max_length=200)
+    transcription = models.TextField(blank=True, null=False)
+
+class Sculpture(ArchiveDocument):
+    MAT_OTHER = 'other'
+    MAT_PLASTIC = 'plastic'
+    MAT_CERAMIC = 'ceramic'
+    MAT_GLASS = 'glass'
+    MAT_METAL = 'metal'
+
+    MATERIAL_CHOICES = [(MAT_OTHER, "Other"),
+                        (MAT_PLASTIC, "Plastic"),
+                        (MAT_CERAMIC, "Ceramic"),
+                        (MAT_GLASS, "Glass"),
+                        (MAT_METAL,"Metal")]
+    material = models.CharField(max_length=50, choices=MATERIAL_CHOICES, default=MAT_METAL)   
     location = models.CharField(max_length=200, blank = True)
     installed_year = models.CharField(max_length=4, blank = True)
     designer = models.CharField(max_length=20, blank = True)
     sculptor = models.CharField(max_length=20, blank = True)
     #size: length, width, height (cm)
-    
-    model3d = models.URLField(max_length=500, blank="True")
-
-
-# class Document(ArchiveDocument):
-#     # might want to do something to standardize this later so people can't
-#     # just enter variant spellings for language names--a preformated list of standard names
-#     # and codes?
-#     language = models.CharField(max_length=200)
-#     transcription = models.TextField(blank=True, null=False)
