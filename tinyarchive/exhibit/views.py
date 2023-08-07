2d8fc3837c4e1a85465f0d4bb4ebacb41e369b5f
anoushkasingal <asingal@berkeley.edu>
2023-08-06 17:34:41 -0700

    testing merge

--- tinyarchive/exhibit/views.py
+++ tinyarchive/exhibit/views.py
@@ -25,6 +25,7 @@ def exhibit_index(request):
     context["exhibits"] = exhibits
     return render(request, "exhibit/index.html", context)
 
+# testing merge
 
 def exhibit_detail(request, exhibit_id):
     context = {}
